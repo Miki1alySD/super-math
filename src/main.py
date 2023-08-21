adb030e890ed5a76031f48726e289a9ffc684af3
Miki_alySD <jack.reacher95@yandex.ru>
2023-08-21 14:33:26 +0400

    tas

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 import modules.perimeter as perimeter
 import modules.factorial as factorial
 
-f3 = factorial.factorial_rec(5)
-print(f3)
-
-f4 = factorial.factorial_rec(15)
-print(f4)
-
 f1 = factorial.factorial_norm(5)
 print(f1)
 
 f2 = factorial.factorial_norm(17)
 print(f2)
+
+f3 = factorial.factorial_rec(5)
+print(f3)
+
+f4 = factorial.factorial_rec(15)
+print(f4)
 
 p1 = perimeter.perimeter_func([1, 2, 5])
 print(p1)
